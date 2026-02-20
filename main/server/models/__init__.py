e76391182ece50924cb6c675cc833fb04a76756e
Earl Jasper Aquitania <earljasper21@gmail.com>
2026-02-20 15:08:15 +0800

    changes on server

--- main/server/models/__init__.py
+++ main/server/models/__init__.py
@@ -1,8 +1,1 @@
-from .users_model import Users
-from .lost_reports_model import LostReports
-from .found_reports_model import FoundReports
-from .success_claimed_model import SuccessClaimed
-from .user_logs_model import UserLog
-from .submit_claims_model import SubmitClaims
-from .verified_claims_model import VerifiedClaims
-
+from .reports_model import Reports
